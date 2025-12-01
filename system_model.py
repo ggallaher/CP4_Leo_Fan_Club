25aa4a7882038121115737272a6db058f72dc7af
Quinn Gossett <lego9156@colorado.edu>
2025-12-01 14:04:41 -0700

    added system model

---
+++ system_model.py
@@ -0,0 +1,16 @@
+import math as m
+import numpy as np
+import control as c
+
+# given parameters
+m1=1 # kg, mass of cart (M)
+m2=0.5 # kg, mass at end of rod (m)
+l=0.4 # m, length of pendulum
+b=0.01 # Ns/m, friction constant 
+g=9.8 # m/s^2, gravity 
+
+
+
+
+
+
