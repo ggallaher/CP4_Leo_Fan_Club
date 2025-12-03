58aa74b8954dea097ccfd1fd3fcecaa5b327a8e6
Joseph Hanley <joseph18106@comcast.net>
2025-12-03 11:54:26 -0700

    Finished animation

--- cp4.py
+++ cp4.py
@@ -1,5 +1,5 @@
 # %% Needed for interactive plot
-%matplotlib tk
+# matplotlib tk
 
 # %% Importing libraries
 import numpy as np
@@ -7,31 +7,59 @@ import control as c
 import matplotlib.pyplot as plt
 import matplotlib.animation as a
 from matplotlib.patches import Rectangle
-from sympy import solveset, var, init_printing, pprint, latex, symbols, sin, cos, pi, Matrix, FiniteSet, nonlinsolve
+from matplotlib.lines import Line2D
+
+plt.close('all')
 
 # %% Q1 Animation attempt
-xmin = -5
-xmax = 5
+xmin = -20
+xmax = 20
 fig, ax = plt.subplots()
-plt.axis("equal")
-xdata, ydata = [1,2,3,4],[0,0,0,0]
-r = Rectangle((0, 0), 0.75, 0.5, linewidth=1, edgecolor='k', facecolor='k')
-x, y = [1,2,3,4],[0,0,0,0]
-r = Rectangle((0, 0), 0.1, 0.2, linewidth=1, edgecolor='k', facecolor='k')
-plt.axis('off')
-plt.hlines(0,xmin, xmax, colors='k')
+ax.set_aspect('equal', adjustable='box')
+
+xdata = [1,2,3,4]
+ydata = [0]*len(xdata)
+
+## Cart
+width, height = 5.0, 2.0
+r = Rectangle((0, 0), width, height, linewidth=1, edgecolor='k', facecolor='k')
+
+## Pendulum
+pendulum_length = 10
+theta = [0, np.pi/8, np.pi/4, 3*np.pi/8]
+pendulum_line = Line2D([], [], linewidth=5, color='#F2DC18')
 
 def init():
-    # ax.set_xlim(xmin, xmax)
-    # ax.set_ylim(-1, 1)
+    ax.cla()                      
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(-1, 15)
+    ax.axis('off')
+    ax.hlines(0, xmin, xmax, colors='k')
+
     ax.add_patch(r)
-    return r,
+    ax.add_line(pendulum_line)
+    return r, pendulum_line
 
 def update(frame):
+    # Move the cart
     r.set_xy([xdata[frame], ydata[frame]])
-    return r,
 
-ani = a.FuncAnimation(fig, update, frames=len(xdata), init_func=init, blit=True)
+    # Top center of the cart
+    x0, y0 = r.get_xy()
+    px = x0 + width / 2
+    py = y0 + height
+
+    # Pendulum angle
+    angle = theta[frame]
+
+    # Tip of the pendulum
+    x_tip = px + pendulum_length * np.sin(angle)
+    y_tip = py + pendulum_length * np.cos(angle)
+
+    pendulum_line.set_data([px, x_tip], [py, y_tip])
+
+    return r, pendulum_line
+
+ani = a.FuncAnimation(fig, update, frames=len(xdata),
+                      init_func=init, blit=True)
 plt.show()
-
-# %%
